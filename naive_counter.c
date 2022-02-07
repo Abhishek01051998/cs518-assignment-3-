5e2a1b22434e0e976aef84696784aa0477a3f3c3
Abhishek <abhishek.jain1@rutgers.edu>
2022-02-07 14:20:27 -0500

    final commit

---
+++ naive_counter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+/*
+1. Generate n threads using pthread library
+2. For every thread call a function(every x times) that increments counter by 1
+*/
+
+#define COUNTER_VALUE (1UL << 24)
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
